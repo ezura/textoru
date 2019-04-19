626253097f5e35c687f29755b3d7d5b013d070e4
ezura <yuka.ezura@gmail.com>
2019-04-19 22:39:45 +0900

    Write dependencied

--- Package.swift
+++ Package.swift
@@ -5,16 +5,24 @@ import PackageDescription
 
 let package = Package(
     name: "textoru",
+    products: [
+        .executable(name: "textoru", targets: ["textoru"])
+    ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/JohnSundell/Files.git", from: "2.2.1"),
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", .branch("master")),
+        .package(url: "https://github.com/apple/swift-syntax.git", .exact("0.50000.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "textoru",
-            dependencies: []),
+            dependencies:  [
+                "Files",
+                "ShellOut",
+                "SwiftSyntax",
+            ]),
         .testTarget(
             name: "textoruTests",
             dependencies: ["textoru"]),
