740672f9ebb26a696cfba9d3f946427aac52706f
ezura <yuka.ezura@gmail.com>
2019-04-19 22:35:30 +0900

    `swift package init --type executable`

---
+++ README.md
@@ -0,0 +1,3 @@
+# textoru
+
+A description of this package.
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import textoruTests
+
+var tests = [XCTestCaseEntry]()
+tests += textoruTests.allTests()
+XCTMain(tests)
